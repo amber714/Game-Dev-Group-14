53fbc65fed7f94302e5586e6877b2e8f0a61304b
MagicShmagic <35780303+MagicShmagic@users.noreply.github.com>
2021-11-29 15:07:18 -0600

    [Update] Generator Fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
@@ -26,10 +28,12 @@ public class GameController : MonoBehaviour
 	
 	public int game_coins;
 	public int game_wave = 1;
+	public int game_playerHealth = 100;
 	
 	public int player_speed = 5;
 	public float player_jump = 8.0F;
 	
+	public int game_gameState = 0; //States are [0,1,2,3] ... 0: startup, 1: running, 2: paused, 3: failed
 	public int game_enemyTotal = 4;
 	public int game_enemyRemaining = 4;
 	
@@ -43,38 +47,100 @@ public class GameController : MonoBehaviour
 	
 	void Start()
     {
+		
 		game_wavePassed = this.GetComponent<AudioSource>();
 		
-		waveText.text = "Wave: " + wave;
-		
-		spawningEnemies = 0; //Default 1
 		Time.timeScale = 1;
+	
+		pauseMenu.SetActive(false);
+		failMenu.SetActive(false);
 		
-		spawnEnemies();
     }
 	
 	void Update()
 	{
-		if (remainingEnemies <= 0) {
+		
+		coinsText.text = "" + game_coins;
+		heartsText.text = "" + game_hearts;
+		
+		if (game_gameState == 0) {
+			
+			if (game_time_countdown > 0) {
+				waveText.text = "Time to start: " + (int)game_time_countdown;
+				enemyText.text = "";
+				game_time_countdown -= Time.deltaTime;
+			} 
+			
+			if (game_time_countdown <= 0) {
+				game_gameState = 1;
+				spawnEnemies();
+			}
+			
+		}
+		
+		if (game_gameState == 1) {
+			enemyText.text = "Enemies: " + game_enemyRemaining;
+			waveText.text = "Wave: " + game_wave;
+		}
+		
+		if (game_enemyRemaining <= 0) {
 			advanceWave();
 		}
+		
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Pause();
+		}
+		
+		if (game_playerHealth <= 0){
+			GameOver();
+		}
+		
+	}
+	
+	public void GameOver()
+    {
+		failMenu.SetActive(true);
+		this.game_gameState = 3;
+		Time.timeScale = 0;
+    }
+	
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	
+	public void RestartLevel()
+	{
+		SceneManager.LoadScene("Game");
+	}
+	
+	public void Pause()
+	{
+		pauseMenu.SetActive(true);
+		this.game_gameState = 2;
+		Time.timeScale = 0;
+	}
+	
+	public void unPause()
+	{
+		pauseMenu.SetActive(false);
+		this.game_gameState = 1;
+		Time.timeScale = 1;
 	}
 	
 	private void advanceWave()
     {
-		Debug.Log("[DEBUG] advanceWave()");
-		wave += 1;
-		enemyCount = (2+(wave*2));
-		remainingEnemies = enemyCount;
+		game_wave += 1;
+		game_enemyTotal = (2+(game_wave*2));
+		game_enemyRemaining = game_enemyTotal;
 		spawnEnemies();
     }
 	
 	private void spawnEnemies()
 	{
-		GameObject[] enemyArray = new GameObject[] { enemy1, enemy2 };
-		Debug.Log("enemyCount: " + this.enemyCount);
-		for (int i = 0; i < this.enemyCount; i++)
-			Instantiate(enemyArray[Random.Range(0, enemyArray.Length)], new Vector3(Random.Range(-20, 20), 1, Random.Range(-20, 20)), Quaternion.identity);
+		GameObject[] game_enemyArray = new GameObject[] { enemy1, enemy2, enemy3, enemy4 };
+		for (int i = 0; i < this.game_enemyTotal; i++)
+			Instantiate(game_enemyArray[Random.Range(0, game_enemyArray.Length)], new Vector3(Random.Range(-20, 20), 1, Random.Range(-20, 20)), Quaternion.identity);
 	}
 	
 }

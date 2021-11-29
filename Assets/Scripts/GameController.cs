454dbbe0e7cc923403ba2f6aba63bdc56d7c9fd8
MagicShmagic <35780303+MagicShmagic@users.noreply.github.com>
2021-11-29 14:48:59 -0600

    [Update] Icon / Hud / Scripting

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,20 +6,50 @@ public class GameController : MonoBehaviour
 {
 	[SerializeField] private GameObject enemy1;
 	[SerializeField] private GameObject enemy2;
+	[SerializeField] private GameObject enemy3;
+	[SerializeField] private GameObject enemy4;
 	
-	public int speed = 5;
-	public int wave = 1;
-	public int enemyCount = 4;
-	public int remainingEnemies = 4;
+	[SerializeField] private GameObject powerup1;
+	[SerializeField] private GameObject powerup2;
+	[SerializeField] private GameObject powerup3;
+	[SerializeField] private GameObject powerup4;
+	
+	[SerializeField] private Text waveText;
+	[SerializeField] private Text enemyText;
+	[SerializeField] private Text coinsText;
+	[SerializeField] private Text healthText;
+	
+	[SerializeField] public GameObject pauseMenu;
+	[SerializeField] public GameObject failMenu;
+	
+	[SerializeField] private GameObject player;
+	
+	public int game_coins;
+	public int game_wave = 1;
+	
+	public int player_speed = 5;
+	public float player_jump = 8.0F;
+	
+	public int game_enemyTotal = 4;
+	public int game_enemyRemaining = 4;
+	
 	public float jumpIntensity = 5f;
 	
-    // Start is called before the first frame update
-    void Awake() {
-		Cursor.visible = false;
-	}
+	private float game_time_countdown = 10;
+	private float game_time_current;
+	private	float game_time_delay = 0.5f;
+	
+	private AudioSource game_wavePassed;
 	
 	void Start()
     {
+		game_wavePassed = this.GetComponent<AudioSource>();
+		
+		waveText.text = "Wave: " + wave;
+		
+		spawningEnemies = 0; //Default 1
+		Time.timeScale = 1;
+		
 		spawnEnemies();
     }
 	

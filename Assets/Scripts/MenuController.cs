70b08cb89aabf9c36ec0a64451da6460335ba4da
MagicShmagic <35780303+MagicShmagic@users.noreply.github.com>
2021-12-05 20:30:52 -0600

    [Update] Title / Scripts

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Time.timeScale = 1;
+    }
+	
+	public void LoadGame()
+	{
+		SceneManager.LoadScene("stage_green");
+	}
+}
